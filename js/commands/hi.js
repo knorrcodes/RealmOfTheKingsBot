9d6ff4b3cacd440f59850e511952cab82ab6d636
knorrcodes <47395767+knorrcodes@users.noreply.github.com>
2022-02-02 00:08:48 -0500

    got the medal.tv to work and grab me and aarons latest clip. need to add in jon and whoever else.

--- js/commands/hi.js
+++ js/commands/hi.js
@@ -1,7 +1,12 @@
+const { Guild } = require("discord.js")
+
 module.exports = {
     name: 'hi',
     description: 'hi',
     execute(msg, args) {
-        msg.channel.send("Hello");
+        const guild = client.guilds.fetch('304745620411777024')
+        const members = guild.members.fetch() // returns Collection
+        console.log(members)
+
     }
 }
--- js/commands/medal.js
+++ js/commands/medal.js
@@ -4,14 +4,32 @@ const fetch = require('cross-fetch');
 module.exports = {
     name: 'medal',
     description: 'medal.tv latest clip',
-    url: 'https://developers.medal.tv/v1/latest?userId=45245258&limit=1',
-    auth: 'priv_7If9072CxwaDRD7HpmKjWJpx4hbBYRFH',
 	execute(msg, args) {
+
+        const map1 = new Map();
+        
+        map1.set('304745620411777024','45245258' );
+        console.log(map1)
+
+        aaronId = '41045699';
+        darrinId = '45245258';
+        jonId = '46512626';
+
+        //me
+        if (msg.author.id == '304745620411777024'){
+            url = 'https://developers.medal.tv/v1/latest?userId=45245258&limit=1'
+        }
+        //aaron
+        else if (msg.author.id == '328204241657397249') {
+            url = 'https://developers.medal.tv/v1/latest?userId=45245258&limit=1'
+        }
+
+        auth = 'priv_7If9072CxwaDRD7HpmKjWJpx4hbBYRFH';
         fetch(
-            this.url,
+            url,
             {
                 headers:{
-                    'Authorization': this.auth
+                    'Authorization': auth
                 }
             }
             )
