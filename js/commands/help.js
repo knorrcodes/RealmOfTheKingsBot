8a7a51c270998cef73cdcd20b4008d8a6964af85
knorrcodes <47395767+knorrcodes@users.noreply.github.com>
2022-02-02 12:18:33 -0500

    updated and fixed the help command. needed to change the way the embed was being sent.

--- js/commands/help.js
+++ js/commands/help.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'help',
 	description: 'help',
@@ -6,32 +8,44 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
                 .setColor('#0000FF')
                 .setTitle('Here are a list of commands to use')
-                .setAuthor('Test bot')
-                .addFields({
+                .setAuthor('Realm of The Kings - Realm Bot')
+                .addFields(
+                {
                     name: '//help',
                     value: 'The help command will return a list of commands that you can use'
-                }, {
+                },
+                {
+                    name: '//medal',
+                    value: `This will grab your most recent clip from Medal.tv and post to the clips and vids channel. 
+                            Reach out to @Dominator of Degenerates if you have not been registered for the command yet`
+                },
+                {
                     name: '//ping',
                     value: 'The ping command will return with the best possible answer'
-                }, {
+                },
+                {
                     name: '//dm',
                     value: 'Send a DM on behalf of Realm Bot'
-                }, {
+                },
+                {
                     name: '//tuff',
                     value: 'You dont want to find out'
-                }, {
+                },
+                {
                     name: '//findmeanewfriend',
                     value: 'You really dont want to find this one out'
-                }, {
+                },
+                {
                     name: '//dadjoke',
                     value:'Recieve a random dad joke'
-                } )
+                }
+                )
                 .setTimestamp()
                 .setFooter('Realm bot gives you kisses');
-            msg.reply(embed);
+            msg.channel.send(embed);
         }
         catch (err){
-            error_messages(err)
+            console.log(err)
         }
 	},
 };
